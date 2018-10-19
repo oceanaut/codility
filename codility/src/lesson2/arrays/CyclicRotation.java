76723fc4f4bbab060031826470e2648b3de37496
oceanaut <mavieksen@gmail.com>
2018-10-19 22:54:32 +0300

    CyclicRotation java

---
+++ codility/src/lesson2/arrays/CyclicRotation.java
@@ -0,0 +1,46 @@
+package lesson2.arrays;
+
+import java.util.Arrays;
+
+public class CyclicRotation {
+	
+	public static int[] solution(int arr[],int K) {
+		int retA[] = new int[arr.length];
+		
+//		int shiftCount=(arr.length+K) % arr.length;
+//		if (shiftCount>0) {
+//			for (int i = 0; i < arr.length; i++) {
+//				int a=(i+K)%arr.length;
+//				retA[a]=arr[i];
+//			}
+//		}else {
+//			return arr;
+//		}
+		if (arr.length==0) {
+			return arr;
+		}else {
+			for (int i = 0; i < arr.length; i++) {
+			int a=(i+K)%arr.length;
+			retA[a]=arr[i];
+		}
+			
+		}
+		
+		return retA;
+	}
+	
+
+	public static void main(String[] args) {
+		//the function should return [6, 3, 8, 9, 7].
+		System.out.println(Arrays.toString(solution(new int[] {3, 8, 9, 7, 6 },1)));
+		//the function should return [9, 7, 6, 3, 8].
+		System.out.println(Arrays.toString(solution(new int[] {3, 8, 9, 7, 6 },3)));
+		//the function should return [0, 0, 0]
+		System.out.println(Arrays.toString(solution(new int[] {0, 0, 0 },1)));
+		//the function should return [1, 2, 3, 4]
+		System.out.println(Arrays.toString(solution(new int[] {1, 2, 3, 4 },4)));
+		 
+
+	}
+
+}
