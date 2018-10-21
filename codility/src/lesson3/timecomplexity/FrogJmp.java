a5d27095ec7911f759f277ac0de0adec3ef41583
oceanaut <mavieksen@gmail.com>
2018-10-22 01:03:28 +0300

    FrogJmp correction

--- codility/src/lesson3/timecomplexity/FrogJmp.java
+++ codility/src/lesson3/timecomplexity/FrogJmp.java
@@ -1,12 +1,13 @@
-package FrogJmp;
+package lesson3.timecomplexity;
 //https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
 //Detected time complexity:O(1)
 
-public class FrogJmp2 {
-
+public class FrogJmp {
+	
 	public static int solution(int X, int Y, int D) {
-		return (int)Math.ceil((double)(Y-X)/D);
+	    return  ((Y - X) % D > 0 ? 1 : 0) + ((Y - X) / D);		
 	}
+
 	public static void main(String[] args) {
 		System.out.println(solution(10, 85, 30));
 
--- codility/src/lesson3/timecomplexity/FrogJmp2.java
+++ codility/src/lesson3/timecomplexity/FrogJmp2.java
@@ -1,13 +1,12 @@
-package FrogJmp;
+package lesson3.timecomplexity;
 //https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
 //Detected time complexity:O(1)
 
-public class FrogJmp {
-	
-	public static int solution(int X, int Y, int D) {
-	    return  ((Y - X) % D > 0 ? 1 : 0) + ((Y - X) / D);		
-	}
+public class FrogJmp2 {
 
+	public static int solution(int X, int Y, int D) {
+		return (int)Math.ceil((double)(Y-X)/D);
+	}
 	public static void main(String[] args) {
 		System.out.println(solution(10, 85, 30));
 
