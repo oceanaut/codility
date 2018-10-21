decfa90da78c555dfdc1ae5cfc0a16bbfe496084
oceanaut <mavieksen@gmail.com>
2018-10-22 01:01:19 +0300

    FrogJmp

---
+++ codility/src/FrogJmp/FrogJmp.java
@@ -0,0 +1,16 @@
+package FrogJmp;
+//https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+//Detected time complexity:O(1)
+
+public class FrogJmp {
+	
+	public static int solution(int X, int Y, int D) {
+	    return  ((Y - X) % D > 0 ? 1 : 0) + ((Y - X) / D);		
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(10, 85, 30));
+
+	}
+
+}
---
+++ codility/src/FrogJmp/FrogJmp2.java
@@ -0,0 +1,15 @@
+package FrogJmp;
+//https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
+//Detected time complexity:O(1)
+
+public class FrogJmp2 {
+
+	public static int solution(int X, int Y, int D) {
+		return (int)Math.ceil((double)(Y-X)/D);
+	}
+	public static void main(String[] args) {
+		System.out.println(solution(10, 85, 30));
+
+	}
+
+}
